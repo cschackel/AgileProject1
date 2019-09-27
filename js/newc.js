b857f3a380f0a9742eec572af0829ec6bbc6f26f
CLS <cschackel@my.wctc.edu>
2019-09-27 17:57:49 -0500

    First Draft Done

---
+++ js/newc.js
@@ -0,0 +1,16 @@
+$(function() {
+
+    $("#certBtn").on('click',function()
+    {
+        $("#certBtn").css('display','none');
+        //alert();
+        $('#certArea').css('display','block');
+    });
+
+    $("#certBtn2").on('click',function()
+    {
+        $("#certBtn2").css('display','none');
+        //alert();
+        $('#certArea2').css('display','block');
+    });
+});
